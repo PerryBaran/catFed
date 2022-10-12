7eecf0ba6d8e2e666b263a7451fd3f61b4cc88e8
Perry <petreli1@ymail.com>
2022-10-12 13:07:48 +0100

    finished in lecutre but forgot to push changes to github

--- src/app.js
+++ src/app.js
@@ -1,11 +1,66 @@
 const express = require('express');
+const { Cat } = require('./models');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/cats', async (req, res) => {
-  res.status(201).send();
-})
+  try {
+    const cat = await Cat.create(req.body);
+    res.status(201).json(cat);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.get('/cats', async (req, res) => {
+  try {
+    const cats = await Cat.findAll({ where: req.query });
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(400).json(err);
+  } 
+});
+
+app.get('/cats/:catId', async (req, res) => {
+  try {
+    const cat = await Cat.findByPk(req.params.catId);
+    res.status(200).json(cat);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.patch('/cats/:catId', async (req, res) => {
+  try {
+    await Cat.update(req.body, { where: { id: req.params.catId } });
+    res.status(200).send();
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.delete('/cats/:catId', async (req, res) => {
+  try {
+    await Cat.destroy({ where: { id: req.params.catId } });
+    res.status(200).send();
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.patch('/feed/:catId', async (req, res) => {
+  try {
+    const affected = await Cat.update({ lastFed: new Date()}, { where: { id: req.params.catId } });
+    if (!affected[0]) {
+      res.status(404).send()
+    } else {
+      res.status(200).json(affected);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = app;
--- src/models/cats.js
+++ src/models/cats.js
@@ -1,4 +1,4 @@
-module.exports = (connection, Sequelize) => {
+module.exports = (sequelize, DataTypes) => {
   const schema = {
     name: DataTypes.STRING,
     breed: DataTypes.STRING,
